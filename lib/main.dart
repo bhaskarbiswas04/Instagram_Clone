36d42f46229325294daf5c30484223bf7c21ca6c
bhaskarbiswas04 <bhaskarb45nov@gmail.com>
2024-05-14 13:58:22 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyAfzaU_2xQS07hbeVwH8XomIocep7JORvk',
+            appId: "1:441122495027:web:fe83e74ae2cb3afd326146",
+            messagingSenderId: "441122495027",
+            projectId: "instagram-clone-2a184"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MainApp());
 }
 
